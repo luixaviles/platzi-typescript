874aa08cc286ff474ac500ca7b03ee6042f5494f
luixaviles <luis.avilesz@gmail.com>
2020-03-10 16:44:31 -0500

    Null y Undefined

---
+++ src/type-null-undefined.ts
@@ -0,0 +1,30 @@
+// Explicita
+let nullVariable: null;
+nullVariable = null;
+// nullVariable = 1; // Error!
+
+let otherVariable = null;
+otherVariable = 'test';
+
+console.log('nullVariable', nullVariable);
+console.log('otherVariable', otherVariable);
+
+// Undefined
+let undefinedVariable: undefined = undefined;
+// undefinedVariable = 'test'; // Error
+
+let otherUndefined = undefined;
+otherUndefined = 1;
+
+console.log('undefinedVariable', undefinedVariable);
+console.log('otherUndefined', otherUndefined);
+
+// Null y Undefined: Como subtipos
+
+// --strictNullChecks
+let albumName: string;
+
+// albumName = null;
+// albumName = undefined;
+
+
