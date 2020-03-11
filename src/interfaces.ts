38d33c22df13b45d8309df6b45f5a0c4cead6b58
luixaviles <luis.avilesz@gmail.com>
2020-03-11 16:09:19 -0500

    Entendiendo Interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,35 @@
+// Funcion para mostrar una Fotografia
+export {};
+
+enum PhotoOrientation {
+    Landscape,
+    Portrait,
+    Square,
+    Panorama
+}
+
+interface Picture {
+    title: string;
+    date: string;
+    orientation: PhotoOrientation
+}
+
+function showPicture(picture: Picture) {
+    console.log(`[title: ${picture.title}, 
+                 date: ${picture.date}, 
+                 orientation: ${picture.orientation}]`);
+}
+
+let myPic = {
+    title: 'Test Title',
+    date: '2020-03',
+    orientation: PhotoOrientation.Landscape
+};
+
+showPicture(myPic);
+showPicture({
+    title: 'Test Title',
+    date: '2020-03',
+    orientation: PhotoOrientation.Portrait,
+    // extra: 'test'// Error
+});
